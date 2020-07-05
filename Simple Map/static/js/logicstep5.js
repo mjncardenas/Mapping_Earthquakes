08d9b0af1ae9db4013ab94cf467e8ade279da9b0
Michelle Joy Cardenas <michelle@michelles-air.home>
2020-07-05 16:53:04 -0700

    added a legend

--- Simple Map/static/js/logicstep5.js
+++ Simple Map/static/js/logicstep5.js
@@ -37,6 +37,7 @@ let map = L.map('mapid', {
   layers: [streets]
 });
 // Create a legend control object.
+
 let legend = L.control({
     position: "bottomright"
 });
@@ -68,23 +69,7 @@ L.control.layers(baseMaps).addTo(map);
 
 // Retrieve the earthquake GeoJSON data.
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
-// This function returns the style data for each of the earthquakes we plot on
-// the map. We pass the magnitude of the earthquake into a function
-// to calculate the radius.
-function styleInfo(feature) {
-    
-    };
-    
-    return {
-      opacity: 1,
-      fillOpacity: 1,
-      fillColor: "#ffae42",
-      color: "#000000",
-      radius: getRadius(),
-      stroke: true,
-      weight: 0.5
-    };
-  }  
+   
   // This function determines the radius of the earthquake marker based on its magnitude.
 // Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
 function getRadius(magnitude) {

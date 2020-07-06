7f0245d44560840a22b7b3617ea979665b84bf31
Michelle Joy Cardenas <michelle@michelles-air.home>
2020-07-06 15:10:22 -0700

    added finished challenge

--- Simple Map/static/js/Earthquake_Challenge.js
+++ Simple Map/static/js/Earthquake_Challenge.js
@@ -14,20 +14,29 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 	maxZoom: 18,
 	accessToken: API_KEY
 });
-
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
 // Create a base layer that holds both maps.
 let baseMaps = {
   "Streets": streets,
-  "Satellite Streets": satelliteStreets
+  "Satellite Streets": satelliteStreets,
+  "Light": light
 };
 
 //Create the earthquake layer for our map
 let earthquakes = new L.LayerGroup();
 
+// Create the tectonic layer
+let tectonic = new L.LayerGroup();
+
 //We define an object that contains the overlays.
 //This overlay will be visible all the time.
 let overlays = {
-    Earthquakes: earthquakes
+    Earthquakes: earthquakes,
+    Tectonic: tectonic 
 };
 
 // Create the map object with a center, zoom level and default layer.
@@ -37,55 +46,41 @@ let map = L.map('mapid', {
   layers: [streets]
 });
 // Create a legend control object.
+
 let legend = L.control({
-    position: "bottomright"
+  position: "bottomright"
 });
 // Then add all the details fot the legend
 legend.onAdd = function() {
-    let div = L.DomUtil.create("div", "info legend");
-    const magnitudes = [0, 1, 2, 3, 4, 5];
-    const colors = [
-        "#98ee00",
-        "#d4ee00",
-        "#eecc00",
-        "#ee9c00",
-        "#ea822c",
-        "#ea2c2c"
-    ];
+  let div = L.DomUtil.create("div", "info legend");
+  const magnitudes = [0, 1, 2, 3, 4, 5];
+  const colors = [
+      "#98ee00",
+      "#d4ee00",
+      "#eecc00",
+      "#ee9c00",
+      "#ea822c",
+      "#ea2c2c"
+  ];
 // Looping through our intervals to generate a label with a colored square for each interval.
-    for (var i = 0; i < magnitudes.length; i++) {
-        console.log(colors[i]);
-        div.innerHTML +=
-        "<i style='background: " + colors[i] + "'></i> " +
-        magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
-    }
-    return div;
+  for (var i = 0; i < magnitudes.length; i++) {
+      console.log(colors[i]);
+      div.innerHTML +=
+      "<i style='background: " + colors[i] + "'></i> " +
+      magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
+  }
+  return div;
 };
 legend.addTo(map);
 
+
 // Then we add our 'graymap' tile layer to the map.
-L.control.layers(baseMaps).addTo(map);
+L.control.layers(baseMaps, overlays).addTo(map);
 
 // Retrieve the earthquake GeoJSON data.
-d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
-// This function returns the style data for each of the earthquakes we plot on
-// the map. We pass the magnitude of the earthquake into a function
-// to calculate the radius.
-function styleInfo(feature) {
-    
-    };
-    
-    return {
-      opacity: 1,
-      fillOpacity: 1,
-      fillColor: getColor(feature.properties.mag),
-      color: "#000000",
-      radius: getRadius(feature.properties.mag),
-      stroke: true,
-      weight: 0.5
-    };
-    };
-  
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) 
+{
+   
   // This function determines the radius of the earthquake marker based on its magnitude.
 // Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
 function getRadius(magnitude) {
@@ -141,8 +136,16 @@ function getColor(magnitude) {
   //  location of the earthquake after the marker has been created and styled.
     onEachFeature: function(feature, layer) {
         layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
-}
-}).addTo(earthquakes);
+}}).addTo(earthquakes);
+earthquakes.addTo(map);
+});
+d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json").then(function(data) {
+L.geoJson(data,{
+  color: "#ff8c00",
+  weight: 3
+
+}).addTo(tectonic);
+  
 // Then we add the earthquake layer to our map.
-    earthquakes.addTo(map);
+    tectonic.addTo(map);
 });
--- Simple Map/static/js/logicstep3.js
+++ Simple Map/static/js/logicstep3.js
@@ -34,17 +34,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
 // This function returns the style data for each of the earthquakes we plot on
 // the map. We pass the magnitude of the earthquake into a function
 // to calculate the radius.
-function styleInfo(feature) {
-    return {
-      opacity: 1,
-      fillOpacity: 1,
-      fillColor: "#ffae42",
-      color: "#000000",
-      radius: getRadius(),
-      stroke: true,
-      weight: 0.5
-    };
-  }  
+
   // This function determines the radius of the earthquake marker based on its magnitude.
 // Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
 function getRadius(magnitude) {
@@ -101,4 +91,7 @@ style: styleInfo,
   onEachFeature: function(feature, layer) {
   layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
 }
-}).addTo(map);
+}).addTo(earthquakes);
+// Then we add the earthquake layer to our map.
+    earthquakes.addTo(map);
+});
